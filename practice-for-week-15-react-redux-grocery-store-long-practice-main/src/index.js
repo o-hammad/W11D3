30f4d8c39386ce1d3dee48f583451a64e4b7e209
claudiaaziz <claudiahannahh@gmail.com>
2023-11-01 14:20:04 -0400

    phase 2

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { Provider } from 'react-redux';
+import { populateProduce } from "./store/produce";
 
 const store = configureStore();
 
@@ -27,4 +28,9 @@ ReactDOM.render(
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
+}
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.populateProduce = populateProduce;
 }
